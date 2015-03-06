27c201b5123e7ff4e9467a1127a381297445b934
radiodario <dario.villanueva@gmail.com>
2015-03-06 18:49:28 -0500

    enable listen all, use image

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    
+
     clean: ['dist'],
 
     ejs: {
@@ -41,6 +41,8 @@ module.exports = function(grunt) {
 
     connect: {
       options: {
+        host: "0.0.0.0",
+        hostname: "0.0.0.0",
         port: process.env.PORT || 3131,
         base: 'dist/',
       },
@@ -83,9 +85,9 @@ module.exports = function(grunt) {
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  
+
   grunt.registerTask('default', ['clean', 'ejs', 'less', 'browserify', 'copy']);
-  
+
   grunt.registerTask('server', ['default', 'connect', 'watch']);
 
   grunt.registerTask('deploy', ['default', 'gh-pages']);
